313dcca31c7b35b3fbe2ef01643d6e6438e0dfcf
Jaya Harish S <115165495+JsDawg1@users.noreply.github.com>
2024-09-04 04:22:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,12 @@
     const submitThanks = document.querySelector('#submitThanks');
     const emailInput = document.querySelector('#emailInput');
     const thanksModifyP = document.querySelector('#thanksModifyP');
+    const mail = document.querySelector('#mailInsert');
 
     // Function to show the thanksDiv and hide the whiteDiv
 
     function showThanksDiv() {
-        thanksModifyP.innerText = `A confirmation email has been sent to your ${emailInput.value}. Please open it and click the button inside to confirm your subscription`
+        mail.innerText = `${emailInput.value}`;
         whiteDiv.style.display = "none";
         thanksDiv.style.display = "block"
     }
